c8869115ce6b2a5cf1570c4035898ca7c74e9271
Stardust <375670450@qq.com>
2017-06-04 15:05:24 +0800

    06-04 15:05 add recognize function in swift3

---
+++ WeTagUnity/CognitiveAPI.swift
@@ -0,0 +1,10 @@
+//
+//  CognitiveAPi.swift
+//  WeTagUnity
+//
+//  Created by cs on 2017/6/4.
+//  Copyright © 2017年 cs. All rights reserved.
+//
+
+import Foundation
+import WebKit
--- WeTagUnity/ViewController.swift
+++ WeTagUnity/ViewController.swift
@@ -8,8 +8,78 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    
+    private var cognitiveURL:String =  "https://api.cognitive.azure.cn/vision/v1.0/models/celebrities/analyze"
+    
+    private var auth:String = "974c0cbdf8b244c28024aaab33ab2fdb"
+    
+    public func recognize(image: UIImage){
+        
+        if let imageData = UIImageJPEGRepresentation(image, 1) {
+            let encodedData = imageData.base64EncodedString(options: NSData.Base64EncodingOptions(rawValue: 0))
+            
+            var request = URLRequest(url: URL(string: cognitiveURL)!)
+            request.httpMethod = "POST"
+            request.httpBody = encodedData.data(using: .utf8)
+            
+            let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                guard let data = data, error == nil else{
+                    print("error=\(error)")
+                    return
+                }
+                if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                    print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                    print("response = \(response)")
+                }
+                /*
+                 {
+                 "requestId": "3f9d03f9-0404-41d5-8752-967ea242d2a0",
+                 "metadata": {
+                    "width": 1024,
+                    "height": 640,
+                    "format": "Jpeg"
+                 },
+                 "result": {
+                    "celebrities": [
+                    {
+                        "name": "ROBERT DOWNEY JR.",
+                        "faceRectangle": {
+                            "left": 483,
+                            "top": 174,
+                            "width": 52,
+                            "height": 52
+                        },
+                        "confidence": 0.991924942
+                    },
+                    {
+                        "name": "Jeremy Renner",
+                        "faceRectangle": {
+                            "left": 943,
+                            "top": 213,
+                            "width": 45,
+                            "height": 45
+                        },
+                        "confidence": 0.990392566
+                    }
+                 ]
+                 }
+                 }
+                 
+                 */
+                let responseString = String(data: data, encoding: .utf8)
+                print("responseString = \(responseString)")
+                // TODO: 解析response字符串获取celebrities数组内容并更新显示tag
+                let json = try? JSONSerialization.jsonObject(with: (responseString?.data(using: .utf8))!)
+            }
+            task.resume()
+        }
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
